a95032aa1b90bacd2d7d33d235f35f373838a243
Elysia Ong <elysia.owx@gmail.com>
2018-09-14 09:29:09 +0800

    Working code to display list of tags

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Tag from './components/Tag';
+import {
+	authorizeLogin,
+	getAccessToken,
+	getRequestToken,
+	getTags,
+} from './utils/pocket-api.js';
+
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			tags: []
+		};
+
+		this.clickTag = this.clickTag.bind(this);
+	}
+
+	componentDidMount() {
+		var loadTags = (token) => {
+			getTags(token).then(tags => {
+				debugger;
+				this.setState({tags: tags});
+			});
+		};
+
+		// TODO integrate with some db
+		if (!localStorage.getItem('reqToken')) {
+			getRequestToken().then(code => {
+				localStorage.setItem('reqToken', code);
+				authorizeLogin(code);
+			});
+		} else {
+			if (!localStorage.getItem('accToken')) {
+				getAccessToken(localStorage.getItem('reqToken')).then(access_token => {
+					localStorage.setItem('accToken', access_token);
+					loadTags(access_token);
+				});
+			} else {
+				loadTags(localStorage.getItem('accToken'));
+			}
+		}
+	}
+
+	clickTag(name) {
+		console.log(name);
+	}
+
 	render() {
+		const tags = this.state.tags.map(tagname =>
+			<Tag key={tagname} name={tagname} clickTag={this.clickTag}></Tag>
+		);
+
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+					<h1 className="App-title">Pocket: Multitags Filter</h1>
 				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<div className="tags">{tags}</div>
 			</div>
 		);
 	}
